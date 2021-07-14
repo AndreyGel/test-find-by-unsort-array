95b2ae25207a24e1afe3b9b763ccf1329a31237f
Andrey <shchedrin@greensight.ru>
2021-07-14 14:48:39 +0300

    done

---
+++ index.php
@@ -0,0 +1,27 @@
+function findUnsortedSubarray(array $items): array {
+
+$unsorted = $items;
+sort($items);
+
+$left = null;
+$right = null;
+
+foreach($unsorted as $key => $unsortedItem) {
+    if ($unsortedItem !== $items[$key]) {
+        $lastUnmatchedIndex = $key; 
+        
+        if (!$left) {
+            $left = $key;
+        } else {
+            if (isset($unsorted[$key + 1]) || $unsorted[$key + 1] !== $items[$key +1]) {
+                $right = $key;
+            }
+        }
+        
+    }
+}
+
+return [$left, $right];
+}
+
+print_r(findUnsortedSubarray([1, 4, 3, 2, 3, 4]));
